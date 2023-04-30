1530bff2c7910eff0a311c6771af8ae215f6f212
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-04-30 18:54:25 +0200

    quick merge fix

--- AlchemistAssembly/Assets/__Scripts/_Managers/SettingsManager.cs
+++ AlchemistAssembly/Assets/__Scripts/_Managers/SettingsManager.cs
@@ -5,10 +5,12 @@ public class SettingsManager : MonoBehaviour
     [field: SerializeField] private PlayerSettings _playerSettings;
     [field: SerializeField] private PlacementToolSettings _placementToolSettings;
     [field: SerializeField] private GridSettings _gridSettings;
+    [field:SerializeField] private BuildingSettings _buildingSettings;
 
     public static PlayerSettings PlayerSettings => Instance._playerSettings;
     public static PlacementToolSettings PlacementToolSettings => Instance._placementToolSettings;
     public static GridSettings GridSettings => Instance._gridSettings;
+    public static BuildingSettings BuildingSettings => Instance._buildingSettings;
     public static SettingsManager Instance { get; private set; }
 
 
