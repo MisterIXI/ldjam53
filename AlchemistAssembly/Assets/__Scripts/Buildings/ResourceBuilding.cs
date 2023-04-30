23682173cad15188c32bc2ca8d544245b74fd836
OliPohl <oliver@pohl66.de>
2023-04-30 18:01:08 +0200

    first draft of resource building

--- AlchemistAssembly/Assets/__Scripts/Buildings/ResourceBuilding.cs
+++ AlchemistAssembly/Assets/__Scripts/Buildings/ResourceBuilding.cs
@@ -1,13 +1,143 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class ResourceBuilding : MonoBehaviour
 {
-    // find out what resource building u are
-    // on interact open window and show stuff // it has to be personallized
-    // -> player has to set route
-    // count progress for production
-    // if progress -> give yannik ok to ship out
-    // start counting again
+    private BuildingSettings _buildingSettings => SettingsManager.BuildingSettings;
+
+    public enum ResourceType   // change those later to incorp it with yanniks class
+    {
+        Shroom,
+        Water,
+        Crystal,
+        Honey
+    }
+
+    UnityEvent m_SendMinecart;
+
+    private ResourceType _resourceType;         // change those later to incorp it with yanniks class
+    private float _resourceTime;
+    private Sprite _resourceSprite;
+    [field: SerializeField] private GameObject[] routes;
+    private int routeCoutner = 0;
+
+
+    private float timer = 0;
+
+
+    void Start() 
+    {
+        // Finds out what type of Building it is and what time needs to create resource / sprite
+        if(gameObject.tag == "Shroom")
+        {
+            _resourceType = ResourceType.Shroom;
+            _resourceTime = _buildingSettings.ShroomTime;
+            _resourceSprite = _buildingSettings.ShroomSprite;
+        }
+        else if(gameObject.tag == "Water")
+        {
+            _resourceType = ResourceType.Water;
+            _resourceTime = _buildingSettings.WaterTime;
+            _resourceSprite = _buildingSettings.WaterSprite;
+        }     
+        else if(gameObject.tag == "Crystal")
+        {
+            _resourceType = ResourceType.Crystal;
+            _resourceTime = _buildingSettings.CrystalTime;
+            _resourceSprite = _buildingSettings.CrystalSprite;
+        }
+        else if(gameObject.tag == "Honey")
+        {
+            _resourceType = ResourceType.Honey;
+            _resourceTime = _buildingSettings.HoneyTime;
+            _resourceSprite = _buildingSettings.HoneySprite;
+        }
+        else
+        {
+            Debug.LogError("Building: " + gameObject.name + " has no tag!");
+        }
+    }
+
+
+    void Update() 
+    {
+        // lets the building produce its resource
+        ProduceResource();
+    }
+
+
+    private void ProduceResource()
+    {
+        if(routes != null)  // if there are routes availible then start producing
+        {
+            timer += Time.deltaTime;
+
+            // if this buildings panel is showing updates the progress slider
+            if(_buildingSettings.CurrentBuilding == gameObject)
+                _buildingSettings.OutputBar.value = timer / _resourceTime * 100;
+
+            if(timer >= _resourceTime)
+            {
+                SendResource();
+                timer = 0;
+            }
+        }
+    }
+
+
+    private void SendResource()
+    {
+        // resets route 
+        if(routeCoutner >= (routes.Length -1))
+            routeCoutner = 0;
+
+        // invokes an event for minecarts ????
+        m_SendMinecart.Invoke(_resourceType, routes[routeCoutner]);              // DAS EVENT FÜR Yannik ? (_resourceType, destination of route)
+        routeCoutner += 1;
+    }
+
+
+    private void OnInteract() //if building is clicked
+    {
+        _buildingSettings.CurrentBuilding = gameObject;
+        _buildingSettings.BuildingPanel.SetActive(true);
+        _buildingSettings.RecepiePanel.SetActive(false);
+        _buildingSettings.InputPanel.SetActive(false);
+        _buildingSettings.RecepieButtonPanel.SetActive(false);
+
+        _buildingSettings.OutputIcon.GetComponent<Image>().sprite = _resourceSprite;
+
+        // show routes
+    }
+
+    private void OnClose()  // if close button is pressed
+    {
+        _buildingSettings.BuildingPanel.SetActive(false);
+        _buildingSettings.RecepiePanel.SetActive(false);
+
+        // hide routes
+    }
+
+
+    private void OnAddRoute()   // if add route button is pressed
+    {
+        _buildingSettings.BuildingPanel.SetActive(false);
+        _buildingSettings.RecepiePanel.SetActive(false);
+
+        // show route tool
+        // show routes
+        // if building is clicked and route tool
+        // -> set route
+            // -> OnInteract()
+        // if different tool or q -> OnClose()
+    }
+
+
+    private void OnClearRoutes()    // if clear routes button is pressed
+    {
+        routes = null; // im not sure this works yannik
+    }
 }
--- AlchemistAssembly/Assets/__Scripts/_ScriptableObjects/BuildingSettings.cs
+++ AlchemistAssembly/Assets/__Scripts/_ScriptableObjects/BuildingSettings.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "BuildingSettings", menuName = "Settings/BuildingSettings", order = 0)]
 public class BuildingSettings : ScriptableObject
@@ -20,4 +21,39 @@ public class BuildingSettings : ScriptableObject
     [field: SerializeField] [field: Range(0f, 30f)] public float YellowCrystalTime { get; set; } = 5;
     [field: SerializeField] [field: Range(0f, 30f)] public float CrystalHoneyTime { get; set; } = 5;
     [field: SerializeField] [field: Range(0f, 30f)] public float PurplePotTime { get; set; } = 5;
+
+
+    [field: Header("Panel")]
+    [field: SerializeField]public GameObject BuildingPanel;
+    [field: SerializeField]public GameObject RecepiePanel;
+    [field: SerializeField]public GameObject InputPanel;
+    [field: SerializeField]public GameObject RecepieButtonPanel;
+    [field: SerializeField]public GameObject InputGrey1, InputRed1, InputGreen1;
+    [field: SerializeField]public GameObject InputGrey2, InputRed2, InputGreen2;
+    [field: SerializeField]public GameObject InputGrey3, InputRed3, InputGreen3;
+    [field: SerializeField]public Slider OutputBar;
+
+
+    [field: Header("Will be overwritten in Script")]
+    [field: SerializeField]public GameObject CurrentBuilding;
+    [field: SerializeField]public GameObject InputIcon1, InputIcon2, InputIcon3;
+    [field: SerializeField]public GameObject OutputIcon;
+
+
+    [field: Header("Sprites")]
+    [field: SerializeField]public Sprite ShroomSprite;
+    [field: SerializeField]public Sprite WaterSprite;
+    [field: SerializeField]public Sprite CrystalSprite;
+    [field: SerializeField]public Sprite HoneySprite;
+    [field: SerializeField]public Sprite RedPotSprite;
+    [field: SerializeField]public Sprite HoneyWaterSprite;
+    [field: SerializeField]public Sprite BluePotSprite;
+    [field: SerializeField]public Sprite RedHoneySprite;
+    [field: SerializeField]public Sprite YellowPotSprite;
+    [field: SerializeField]public Sprite CrystalHoneySprite;
+    [field: SerializeField]public Sprite PurplePotSprite;
+
+
+    [field: Header("Recepies")]
+    [field: SerializeField]public ResourceBuilding[] RedPot;
 }
