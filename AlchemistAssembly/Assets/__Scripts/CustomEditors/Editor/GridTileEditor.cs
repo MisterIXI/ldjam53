2062f3e94a2c5b3d2792ad708d407a0f47f4d280
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-05-01 21:03:38 +0200

    map creator implemented

--- AlchemistAssembly/Assets/__Scripts/CustomEditors/Editor/GridTileEditor.cs
+++ AlchemistAssembly/Assets/__Scripts/CustomEditors/Editor/GridTileEditor.cs
@@ -25,6 +25,33 @@ public class GridTileEditor : Editor
                 HexGrid.GetTile(neighbour).SetResourceInfo(SettingsManager.GridSettings.TileColors[0], ResourceType.Shroom);
             }
         }
+        if (GUILayout.Button("SetTileTypeWater"))
+        {
+            gridTile.SetResourceInfo(SettingsManager.GridSettings.TileColors[1], ResourceType.Water);
+            var neighbours = HexHelper.GetNeighboursOddQ(gridTile.TileIndex);
+            foreach (var neighbour in neighbours)
+            {
+                HexGrid.GetTile(neighbour).SetResourceInfo(SettingsManager.GridSettings.TileColors[1], ResourceType.Water);
+            }
+        }
+        if (GUILayout.Button("SetTileTypeCrystal"))
+        {
+            gridTile.SetResourceInfo(SettingsManager.GridSettings.TileColors[2], ResourceType.Crystal);
+            var neighbours = HexHelper.GetNeighboursOddQ(gridTile.TileIndex);
+            foreach (var neighbour in neighbours)
+            {
+                HexGrid.GetTile(neighbour).SetResourceInfo(SettingsManager.GridSettings.TileColors[2], ResourceType.Crystal);
+            }
+        }
+        if (GUILayout.Button("SetTileTypeHoney"))
+        {
+            gridTile.SetResourceInfo(SettingsManager.GridSettings.TileColors[3], ResourceType.Honey);
+            var neighbours = HexHelper.GetNeighboursOddQ(gridTile.TileIndex);
+            foreach (var neighbour in neighbours)
+            {
+                HexGrid.GetTile(neighbour).SetResourceInfo(SettingsManager.GridSettings.TileColors[3], ResourceType.Honey);
+            }
+        }
         // add label that shows the index of the tile
         EditorGUILayout.LabelField("Index: " + gridTile.TileIndex);
 
