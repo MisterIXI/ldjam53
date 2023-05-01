8946be30dc7b2dcf9e5ba0975128a0571194a7cc
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-05-01 12:56:33 +0200

    minecart progress commit

--- AlchemistAssembly/Assets/__Scripts/MineCarts/MineCart.cs
+++ AlchemistAssembly/Assets/__Scripts/MineCarts/MineCart.cs
@@ -1,7 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MineCart : MonoBehaviour
 {
+    private List<GridTile> _path;
+    private int _currentPathIndex;
+    private float t;
+    public void Initialize(List<GridTile> path)
+    {
+        _path = path;
+        transform.position = _path.First().transform.position;
+        t = 0.5f;
+    }
+    private void FixedUpdate()
+    {
+
+    }
+
+    // private IEnumerator SpawnAnimation()
+    // {
+    //     transform.localScale = Vector3.zero;
+
+    // }
+
+
+    // private void SpawnAnimation
+
 
     private void OnDestroy()
     {
