b9c0101376809a34c746c6b914b59e4e445b48c7
Yannik BrÃaendle <yannik.braendle@t-online.de>
2023-05-01 22:52:19 +0200

    Minecart fixed

--- AlchemistAssembly/Assets/__Scripts/GridEntities/RailEntity.cs
+++ AlchemistAssembly/Assets/__Scripts/GridEntities/RailEntity.cs
@@ -4,7 +4,7 @@ public class RailEntity : Placeable
 {
     [field: SerializeField] public GameObject RailStraight { get; private set; }
     [field: SerializeField] public GameObject RailCorner { get; private set; }
-    [HideInInspector] public MineCart OccupyingMineCart;
+    public MineCart OccupyingMineCart;
     public override void PlaceOnTile(GridTile tile)
     {
         base.PlaceOnTile(tile);
